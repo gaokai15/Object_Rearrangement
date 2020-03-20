404c377410e33b0de6712dc2fd55c3cf2d15c5ff
Daniel Nakhimovich <dnahimov@gmail.com>
2020-03-20 19:26:40 -0400

    temp

--- cgraph/cgraph.py
+++ cgraph/cgraph.py
@@ -211,55 +211,64 @@ def genCGraph(numObjs, RAD, HEIGHT, WIDTH, display, displayMore, savefile):
                 drawProblem(HEIGHT, WIDTH, wall_mink, env_polys, None, pointStart, pointGoal)
             continue
 
-        if displayMore:
-            drawProblem(HEIGHT, WIDTH, wall_mink_poly, env_polys, None, pointStart, pointGoal)
-
-        env_polys.insert(0, wall_mink_poly)
-
-        env_polys_vis = []
-        for poly in filter(lambda p: wall_mink_poly.covers(p), env_polys):
-            po = vis.Polygon([vis.Point(*p) for p in reversed(pu.pointList(poly))])
-            env_polys_vis.append(po)
-
-        env = vis.Environment(env_polys_vis)
-        if not env.is_valid(epsilon):
-            displayMore = True
-            drawProblem(HEIGHT, WIDTH, wall_mink_poly, env_polys, None, pointStart, pointGoal)
-            if savefile:
-                savefile += ".env_error"
-            else:
-                savefile = "polys.json" + str(time()) + ".env_error"
-
-        start = vis.Point(*points[indStart])
-        goal = vis.Point(*points[indGoal])
-
-        start.snap_to_boundary_of(env, epsilon)
-        start.snap_to_vertices_of(env, epsilon)
-
-        # t0 = time()
-        path = env.shortest_path(start, goal, epsilon)
-        # print(time() - t0)
-
         collides = False
-        # for p1, p2 in zip(path.path()[:-1], path.path()[1:]):
-        #     collides |= not isEdgeCollisionFree(
-        #         polygon, [[p1.x(), p1.y()], [p2.x(), p2.y()]], obstacles
-        #     )
-        #     if collides:
-        #         break
-        # for p in list(path.path()):
-        #     collides |= not isCollisionFree(polygon, [[p.x(), p.y()]], obstacles)
-        #     if collides:
-        #         break
+        if display:
 
-        path = [(p.x(), p.y()) for p in path.path()]
+            if displayMore:
+                drawProblem(
+                    HEIGHT, WIDTH, wall_mink_poly, env_polys, None, pointStart, pointGoal
+                )
+
+            env_polys.insert(0, wall_mink_poly)
+
+            env_polys_vis = []
+            for poly in filter(lambda p: wall_mink_poly.covers(p), env_polys):
+                po = vis.Polygon([vis.Point(*p) for p in reversed(pu.pointList(poly))])
+                env_polys_vis.append(po)
+
+            env = vis.Environment(env_polys_vis)
+            if not env.is_valid(epsilon):
+                displayMore = True
+                drawProblem(
+                    HEIGHT, WIDTH, wall_mink_poly, env_polys, None, pointStart, pointGoal
+                )
+                if savefile:
+                    savefile += ".env_error"
+                else:
+                    savefile = "polys.json" + str(time()) + ".env_error"
+
+            start = vis.Point(*points[indStart])
+            goal = vis.Point(*points[indGoal])
+
+            start.snap_to_boundary_of(env, epsilon)
+            start.snap_to_vertices_of(env, epsilon)
+
+            t0 = time()
+            path = env.shortest_path(start, goal, epsilon)
+            print(time() - t0)
+
+            # for p1, p2 in zip(path.path()[:-1], path.path()[1:]):
+            #     collides |= not isEdgeCollisionFree(
+            #         polygon, [[p1.x(), p1.y()], [p2.x(), p2.y()]], obstacles
+            #     )
+            #     if collides:
+            #         break
+            # for p in list(path.path()):
+            #     collides |= not isCollisionFree(polygon, [[p.x(), p.y()]], obstacles)
+            #     if collides:
+            #         break
+
+            path = [(p.x(), p.y()) for p in path.path()]
+        else:
+            path = [points[indStart], 'exists', points[indGoal]]
+
         if not collides:
             paths[(indStart, indGoal)] = path
             color = 'blue'
         else:
             color = 'red'
 
-        if displayMore:
+        if display and displayMore:
             drawProblem(
                 HEIGHT, WIDTH, wall_mink_poly, env_polys, (path, color), pointStart, pointGoal
             )
@@ -296,6 +305,212 @@ def genCGraph(numObjs, RAD, HEIGHT, WIDTH, display, displayMore, savefile):
             )
 
     return graph, paths
+
+
+# def loadCGraph(savefile, repath, display, displayMore):
+#     if savefile:
+#         with open(savefile, 'w') as finput:
+#             data = json.load(
+#                 {
+#                     'numObjs': numObjs,
+#                     'RAD': RAD,
+#                     'HEIGHT': HEIGHT,
+#                     'WIDTH': WIDTH,
+#                     'points': points,
+#                     'objects': np.array(objects).tolist(),
+#                     # staticObs = []
+#                     'graph': graph,
+#                     'path': {str(k): v
+#                              for k, v in paths.items()},
+#                 },
+#                 finput,
+#             )
+#     numObjs, RAD, HEIGHT, WIDTH, points, objects
+
+#     epsilon = 2**-8
+#     polygon = np.array(poly_disk([0, 0], RAD, 30))
+#     wall_pts = pn.Polygon([(0, 0), (WIDTH, 0), (WIDTH, HEIGHT), (0, HEIGHT)])
+#     wall_mink = pn.Polygon(
+#         [(RAD, RAD), (WIDTH - RAD, RAD), (WIDTH - RAD, HEIGHT - RAD), (RAD, HEIGHT - RAD)]
+#     )
+
+#     points = []
+#     objects = []
+#     # staticObs = []
+#     minkowski_objs = []
+#     # minkowski_poly = []
+#     for i in range(numObjs):
+
+#         for i in range(2):
+#             isfree = False
+#             while not isfree:
+#                 point = (
+#                     uniform(0 - min(polygon[:, 0]), WIDTH - max(polygon[:, 0])),
+#                     uniform(0 - min(polygon[:, 1]), HEIGHT - max(polygon[:, 1]))
+#                 )
+#                 isfree = isCollisionFree(
+#                     polygon * (1 + np.sqrt(0.502)),
+#                     point,
+#                     objects  # [:len(objects) - i]
+#                 )
+#             points.append(point)
+#             objects.append(pn.Polygon(polygon + point))
+#             mink_obj = 2 * polygon + point
+
+#             # delta = epsilon * 0
+#             # for i in range(len(mink_obj)):
+#             #     # if mink_obj[i][0] < 0:
+#             #     #     mink_obj[i][0] = delta
+#             #     # elif mink_obj[i][0] > WIDTH - RAD:
+#             #     #     mink_obj[i][0] = WIDTH - RAD - delta
+
+#             #     if mink_obj[i][0] < RAD:
+#             #         mink_obj[i][0] = RAD + delta
+#             #     elif mink_obj[i][0] > WIDTH - RAD:
+#             #         mink_obj[i][0] = WIDTH - RAD - delta
+
+#             #     # if mink_obj[i][1] < 0:
+#             #     #     mink_obj[i][1] = delta
+#             #     # elif mink_obj[i][1] > HEIGHT - RAD:
+#             #     #     mink_obj[i][1] = HEIGHT - RAD - delta
+
+#             #     if mink_obj[i][1] < RAD:
+#             #         mink_obj[i][1] = RAD + delta
+#             #     elif mink_obj[i][1] > HEIGHT - RAD:
+#             #         mink_obj[i][1] = HEIGHT - RAD - delta
+
+#             minkowski_objs.append(pn.Polygon(mink_obj))
+
+#     if display:
+#         drawProblem(HEIGHT, WIDTH, wall_pts, objects)
+
+#     paths = {}
+#     for indStart, indGoal in combinations(range(numObjs * 2), 2):
+
+#         robotStart = pu.pointList(objects[indStart])
+#         robotGoal = pu.pointList(objects[indGoal])
+#         pointStart = points[indStart] + polygon * 0.1
+#         pointGoal = points[indGoal] + polygon * 0.1
+#         obstacles = objects[:indStart] + objects[indStart + 1:indGoal] + objects[indGoal + 1:]
+#         minkowski_obs = minkowski_objs[:indStart]
+#         minkowski_obs += minkowski_objs[indStart + 1:indGoal]
+#         minkowski_obs += minkowski_objs[indGoal + 1:]
+
+#         if displayMore:
+#             drawProblem(HEIGHT, WIDTH, wall_pts, obstacles, None, robotStart, robotGoal)
+
+#         pm = pn.Polygon(wall_mink)
+#         pm -= sum([pn.Polygon(obj) for obj in minkowski_obs], pn.Polygon())
+#         pm.simplify()
+
+#         wall_mink_poly = None
+#         env_polys = []
+#         for poly in sorted([pn.Polygon(p) for p in pm], key=lambda p: p.area()):
+#             if wall_mink_poly is None:
+#                 if poly.isInside(*points[indStart]) and poly.isInside(*points[indGoal]):
+#                     wall_mink_poly = poly
+#                     continue
+#             env_polys.append(poly)
+
+#         if wall_mink_poly is None:
+#             if displayMore:
+#                 drawProblem(HEIGHT, WIDTH, wall_mink, env_polys, None, pointStart, pointGoal)
+#             continue
+
+#         collides = False
+#         if display:
+
+#             if displayMore:
+#                 drawProblem(
+#                     HEIGHT, WIDTH, wall_mink_poly, env_polys, None, pointStart, pointGoal
+#                 )
+
+#             env_polys.insert(0, wall_mink_poly)
+
+#             env_polys_vis = []
+#             for poly in filter(lambda p: wall_mink_poly.covers(p), env_polys):
+#                 po = vis.Polygon([vis.Point(*p) for p in reversed(pu.pointList(poly))])
+#                 env_polys_vis.append(po)
+
+#             env = vis.Environment(env_polys_vis)
+#             if not env.is_valid(epsilon):
+#                 displayMore = True
+#                 drawProblem(
+#                     HEIGHT, WIDTH, wall_mink_poly, env_polys, None, pointStart, pointGoal
+#                 )
+#                 if savefile:
+#                     savefile += ".env_error"
+#                 else:
+#                     savefile = "polys.json" + str(time()) + ".env_error"
+
+#             start = vis.Point(*points[indStart])
+#             goal = vis.Point(*points[indGoal])
+
+#             start.snap_to_boundary_of(env, epsilon)
+#             start.snap_to_vertices_of(env, epsilon)
+
+#             t0 = time()
+#             path = env.shortest_path(start, goal, epsilon)
+#             print(time() - t0)
+
+#             # for p1, p2 in zip(path.path()[:-1], path.path()[1:]):
+#             #     collides |= not isEdgeCollisionFree(
+#             #         polygon, [[p1.x(), p1.y()], [p2.x(), p2.y()]], obstacles
+#             #     )
+#             #     if collides:
+#             #         break
+#             # for p in list(path.path()):
+#             #     collides |= not isCollisionFree(polygon, [[p.x(), p.y()]], obstacles)
+#             #     if collides:
+#             #         break
+
+#             path = [(p.x(), p.y()) for p in path.path()]
+#         else:
+#             path = [points[indStart], 'exists', points[indGoal]]
+
+#         if not collides:
+#             paths[(indStart, indGoal)] = path
+#             color = 'blue'
+#         else:
+#             color = 'red'
+
+#         if display and displayMore:
+#             drawProblem(
+#                 HEIGHT, WIDTH, wall_mink_poly, env_polys, (path, color), pointStart, pointGoal
+#             )
+#             drawProblem(
+#                 HEIGHT, WIDTH, wall_pts, obstacles, (path, color), robotStart, robotGoal
+#             )
+
+#     if display:
+#         drawConGraph(HEIGHT, WIDTH, paths, objects)
+
+#     graph = {}
+#     for uv, p in paths.items():
+#         u, v = uv
+#         if p is not None:
+#             graph[u] = sorted(graph.get(u, []) + [v])
+#             graph[v] = sorted(graph.get(v, []) + [u])
+
+#     if savefile:
+#         with open(savefile, 'w') as output:
+#             json.dump(
+#                 {
+#                     'numObjs': numObjs,
+#                     'RAD': RAD,
+#                     'HEIGHT': HEIGHT,
+#                     'WIDTH': WIDTH,
+#                     'points': points,
+#                     'objects': np.array(objects).tolist(),
+#                     # staticObs = []
+#                     'graph': graph,
+#                     'path': {str(k): v
+#                              for k, v in paths.items()},
+#                 },
+#                 output,
+#             )
+
+#     return graph, paths
 
 
 if __name__ == "__main__":
