ced83e9ee5cbc3c8363f4c91726408b88d2666c5
Daniel Nakhimovich <dnahimov@gmail.com>
2020-06-26 17:11:43 -0400

    easy build scripts

--- rgraph/get_headers.sh
+++ rgraph/get_headers.sh
@@ -1,5 +1,6 @@
+#!/usr/bin/env bash
 wget https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-5.0.2/CGAL-5.0.2.tar.xz
-tar -xf CGAL-5.0.2.tar.xz
-
 wget https://dl.bintray.com/boostorg/release/1.73.0/source/boost_1_73_0.tar.gz
+
+tar -xf CGAL-5.0.2.tar.xz
 tar -xf boost_1_73_0.tar.gz
