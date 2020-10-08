d253d2ea8229f7b87e509990780e8db926648559
Daniel Nakhimovich <dnahimov@gmail.com>
2020-10-08 18:54:42 -0400

    new tests

---
+++ dgraph/tests/test5.py
@@ -0,0 +1,18 @@
+DiskCSpace(
+    rad=60,
+    height=1000,
+    width=1000,
+    obstacles=[
+        Circle(500, 500, 220),
+    ],
+    poseMap={
+        'S0': Circle(820, 180, 60),
+        'G0': Circle(180, 820, 60),
+        'S1': Circle(180, 740, 60),
+        'G1': Circle(740, 180, 60),
+        'S2': Circle(400, 850, 60),
+        'G2': Circle(600, 850, 60),
+        'S3': Circle(400, 150, 60),
+        'G3': Circle(600, 150, 60),
+    },
+)
