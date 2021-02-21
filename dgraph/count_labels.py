a2b7b13acdc5aae676e76d4f6cfbe243b7505e6f
Daniel Nakhimovich <dnahimov@gmail.com>
2021-02-21 11:43:48 -0500

    count c3

--- dgraph/count_labels.py
+++ dgraph/count_labels.py
@@ -17,10 +17,14 @@ tcc1 = 0
 tcc2 = 0
 bcc1 = 0
 bcc2 = 0
+cc3 = 0
+tcc3 = 0
+bcc3 = 0
 progress = 0
 toprint = []
 dfdata = []
 dfdata2 = []
+dfdata3 = []
 for filename in sorted(glob.glob(sys.argv[1] + '/*/*/*/*.json')):
     D, n, trial = filename.split('/')[2:]
     D = float(D.split('=')[-1])
@@ -29,7 +33,7 @@ for filename in sorted(glob.glob(sys.argv[1] + '/*/*/*/*.json')):
     # print(D, n, trial)
 
     if progress % 1600 == 0 and progress > 0:
-        print("ch1:", cc1, "/", bcc1, "ch2 ", cc2, "/", bcc2, file=sys.stderr)
+        print("ch1:", cc1, "/", bcc1, "ch2 ", cc2, "/", bcc2, "ch3 ", cc3, "/", bcc3, file=sys.stderr)
         cc1 = 0
         cc2 = 0
         bcc1 = 0
@@ -39,7 +43,7 @@ for filename in sorted(glob.glob(sys.argv[1] + '/*/*/*/*.json')):
         data = json.load(f)
     if 'ch1' in filename:
         bcc1 += 1
-        if 'computation_time' in data:
+        if 'is_monotone' in data:
             cc1 += 1
             tcc1 += 1
             dfdata.append([D, n, trial, data['is_monotone'], data['computation_time']])
@@ -49,7 +53,7 @@ for filename in sorted(glob.glob(sys.argv[1] + '/*/*/*/*.json')):
         #     toprint.append(filename)
     if 'ch2' in filename:
         bcc2 += 1
-        if 'computation_time' in data:
+        if 'is_perturbable' in data:
             cc2 += 1
             tcc2 += 1
             if data['is_perturbable'] != 'Timeout':
@@ -60,6 +64,20 @@ for filename in sorted(glob.glob(sys.argv[1] + '/*/*/*/*.json')):
             else:
                 vals = 2  # Timeout
             dfdata2.append([D, n, trial, vals, data['computation_time']])
+
+    if 'ch3' in filename:
+        bcc3 += 1
+        if 'is_valid_buffer' in data:
+            cc3 += 1
+            tcc3 += 1
+            if data['is_valid_buffer'] != 'Timeout':
+                if 'Error' not in data['is_valid_buffer'].values():
+                    vals = sum(data['is_valid_buffer'].values()) / int(n)
+                else:
+                    vals = -1  # Error
+            else:
+                vals = 2  # Timeout
+            dfdata3.append([D, n, trial, vals, data['computation_time']])
 
 pprint(toprint)
 print("Total: ch1:", tcc1, "ch2", tcc2, file=sys.stderr)
@@ -87,3 +105,15 @@ print(
     file=sys.stderr,
 )
 print("Not non-2tone: ", len(df2.query("num_pert>0")))
+columns3 = ["density", "number", "trial", "num_buff", "time"]
+df3 = pd.DataFrame(data=dfdata2, columns=columns2)
+print(
+    '\nTimeouts: ',
+    len(df3.query('num_buff==2')),
+    '\nErrors: ',
+    len(df3.query('num_buff==-1')),
+    '\nTotal: ',
+    len(df3),
+    file=sys.stderr,
+)
+print("Not non-2tone: ", len(df3.query("num_buff>0")))
